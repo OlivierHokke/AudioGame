49c19529fdc8a8f7e665e2bc991f4e10bdaeb5c4
Olivier Hokke <github@wolfox.nl>
2014-11-24 19:17:10 +0100

    Working on NavMesh POC

---
+++ NavMesh Sound Propagation/Assets/NavMesh/NavMeshEdge.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class NavMeshEdge
+{
+    public NavMeshNode target;
+    public bool occlude;
+
+    public NavMeshEdge(NavMeshNode target, bool occlude)
+    {
+        this.target = target;
+        this.occlude = occlude;
+    }
+}
