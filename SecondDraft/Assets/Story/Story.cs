349b7a975b54bead8dfacf0ae21e9572d7cdf622
miguelgazela <miguel.gazela@gmail.com>
2014-12-04 23:20:16 +0100

    Added simple elevator state transition.

--- SecondDraft/Assets/Story/Story.cs
+++ SecondDraft/Assets/Story/Story.cs
@@ -85,6 +85,7 @@ public class Story : MonoBehaviour {
 		RemoveFlatDoor.NextState = FollowLucyToElevator;
 		FollowLucyToElevator.NextState = RemoveElevatorDoor;
 		RemoveElevatorDoor.NextState = ElevatorState;
+		ElevatorState.NextState = LucyExplainsCars;
 	}
 	
 	void Update () 
