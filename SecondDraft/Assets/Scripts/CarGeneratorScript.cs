f6d1d52358b0836ed9831b751b4956b81beaf065
miguelgazela <miguel.gazela@gmail.com>
2014-12-05 01:01:15 +0100

    Added car generators to the streets. The models and behavior are still very simple.

---
+++ SecondDraft/Assets/Scripts/CarGeneratorScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarGeneratorScript : MonoBehaviour {
+
+	public CarScript car; 
+	public GameObject spawnPosition;
+	public GameObject carLiveArea;
+	public float carSpeed;
+
+	private const float MIN_RANDOM_DELAY = 1.0f;
+	private const float MAX_RANDOM_DELAY = 7.0f;
+	private float timeUntilNextCar;
+
+	// Use this for initialization
+	void Start () {
+		timeUntilNextCar = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeUntilNextCar -= Time.deltaTime;
+
+		if (timeUntilNextCar < 0) {
+			timeUntilNextCar = Randomg.Range(MIN_RANDOM_DELAY, MAX_RANDOM_DELAY);
+			CarScript newCar = Instantiate(car, spawnPosition.transform.position, spawnPosition.transform.rotation) as CarScript;
+			newCar.Init(this, carSpeed);
+		}
+	}
+}
---
+++ SecondDraft/Assets/Scripts/CarScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarScript : MonoBehaviour {
+
+	private CarGeneratorScript generator;
+	private float speed;
+
+	public void Init(CarGeneratorScript g, float startingSpeed) {
+		this.generator = g;
+		this.speed = startingSpeed;
+	}
+
+	void FixedUpdate() {
+		transform.position += transform.forward*((speed)*Time.deltaTime);
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other == generator.carLiveArea.collider) {
+			Destroy (gameObject);
+		}
+	}
+}
