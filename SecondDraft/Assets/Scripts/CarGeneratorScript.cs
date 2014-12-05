3668bcb82758f293d7417fd3aae969f537d15195
miguelgazela <miguel.gazela@gmail.com>
2014-12-05 16:10:38 +0100

    Improved the cars mechanism.
    Cars have a simple model and stop once the player gets too close to them. They also stop if there's stopped car in front of them.
    They start moving again as soon as the player gets out of the way.

--- SecondDraft/Assets/Scripts/CarGeneratorScript.cs
+++ SecondDraft/Assets/Scripts/CarGeneratorScript.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class CarGeneratorScript : MonoBehaviour {
 
-	public CarScript car; 
+	public CarScript car;
 	public GameObject spawnPosition;
 	public GameObject carLiveArea;
 	public float carSpeed;
+	public GameObject street;
+	public bool isActive;	
 
 	private const float MIN_RANDOM_DELAY = 1.0f;
 	private const float MAX_RANDOM_DELAY = 7.0f;
@@ -15,16 +17,22 @@ public class CarGeneratorScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		timeUntilNextCar = 0;
+		isActive = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		timeUntilNextCar -= Time.deltaTime;
 
-		if (timeUntilNextCar < 0) {
+		if (timeUntilNextCar < 0 && isActive) {
 			timeUntilNextCar = Randomg.Range(MIN_RANDOM_DELAY, MAX_RANDOM_DELAY);
 			CarScript newCar = Instantiate(car, spawnPosition.transform.position, spawnPosition.transform.rotation) as CarScript;
 			newCar.Init(this, carSpeed);
+			newCar.transform.parent = street.transform;
 		}
+	}
+
+	public void SetActive(bool state) {
+		isActive = state;
 	}
 }
