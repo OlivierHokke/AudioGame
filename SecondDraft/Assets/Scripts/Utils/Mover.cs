a0bf852dd66a26c39d43b83ac781bcc2d81b81e5
Mick van Gelderen <mickvangelderen@gmail.com>
2014-12-12 16:42:14 +0100

    Fixed mover

--- SecondDraft/Assets/Scripts/Utils/Mover.cs
+++ SecondDraft/Assets/Scripts/Utils/Mover.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Rotator : MonoBehaviour {
+public class Mover : MonoBehaviour {
 
 	public float speed = 10.0f;
-	public Vector3 displacement = new Vector3;
-	public Vector3 rotation = new Vector3(1, 1, 1);
+	public Vector3 displacement = new Vector3(1, 1, 1);
+	
+	private Vector3 startPosition;
+
+	void Awake() {
+		startPosition = transform.position;
+	}
 
 	// Update is called once per frame
 	void Update () {
-		transform.rotation = transform.rotation * Quaternion.Euler(rotation * speed * Time.deltaTime);
+		transform.position = startPosition + Mathf.Sin(Time.time * speed) * displacement;
 	}
 }
