3fb46920c07b41da52107ebd4ed74531e47c428d
Olivier Hokke <github@wolfox.nl>
2014-12-18 13:53:38 +0100

    Reworked the controls system added an oculus switch

--- SecondDraft/Assets/Scripts/Player/PlayerController.cs
+++ SecondDraft/Assets/Scripts/Player/PlayerController.cs
@@ -12,11 +12,6 @@ public class PlayerController : MonoBehaviour {
     public float currentLookVertical = 0f;
     public float currentLookHorizontal = 0f;
 
-    public bool oculusRiftEnabled;
-    public GameObject oculusRiftRoot;
-    public GameObject oculusRiftCamera;
-    public GameObject normalRoot;
-    public GameObject normalCamera;
 
     public event EventHandler<TriggerEventArgs> TriggerEntered;
     public event EventHandler<TriggerEventArgs> TriggerExit;
@@ -26,37 +21,10 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        Vector3 fw;
-        if (oculusRiftEnabled)
-        {
-            if (!oculusRiftRoot.activeInHierarchy) oculusRiftRoot.SetActive(true);
-            if (normalRoot.activeInHierarchy) normalRoot.SetActive(false);
-            PlayerCompass.SetSource(oculusRiftCamera);
-            fw = oculusRiftCamera.transform.TransformDirection(Vector3.forward);
-        }
-        else
-        {
-            if (oculusRiftRoot.activeInHierarchy) oculusRiftRoot.SetActive(false);
-            if (!normalRoot.activeInHierarchy) normalRoot.SetActive(true);
-            PlayerCompass.SetSource(normalCamera);
-            fw = normalCamera.transform.TransformDirection(Vector3.forward);
-        }
-
+        BaseControls c = ControlsManager.current;
         if (!LockMovement)
-        {
-            fw = fw.sety(0).normalized;
-            transform.Translate(fw * Time.deltaTime * walkUnitsPerSecond * Input.GetAxis("Vertical"), Space.Self);
-        }
-
-        currentLookHorizontal += Time.deltaTime * turnUnitsPerSecond * Input.GetAxis("Horizontal");
-        currentLookHorizontal += Input.GetAxis("Mouse X") * anglesPerPixel;
-        normalRoot.transform.localEulerAngles = new Vector3(0f, currentLookHorizontal, 0f);
-        normalRoot.transform.localEulerAngles = new Vector3(0f, currentLookHorizontal, 0f);
-
-        //normalRoot.transform.rotation = normalRoot.transform.rotation * Quaternion.AngleAxis(Input.GetAxis("Mouse X") * anglesPerPixel, Vector3.up);
-        /*currentLookVertical += Input.GetAxis("Mouse Y") * anglesPerPixel;
-        currentLookVertical = Mathf.Clamp(currentLookVertical, minLookVertical, maxLookVertical);
-        camera.transform.localEulerAngles = -Vector3.right * currentLookVertical;*/
+        transform.position += c.GetMove();
+        transform.rotation *= c.GetRotation();
 	}
 
     void OnDrawGizmos()
