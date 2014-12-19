da1695b7efea54c56164de4e629a9bedebd7f012
Olivier Hokke <github@wolfox.nl>
2014-12-19 20:31:32 +0100

    FPS Controls are done

--- SecondDraft/Assets/Controls/BaseControls.cs
+++ SecondDraft/Assets/Controls/BaseControls.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public abstract class BaseControls
 {
     public abstract void OnEnable();
-    public abstract Vector3 GetMove();
-    public abstract Quaternion GetRotation();
+    public abstract Vector3 GetMove(Vector3 current);
+    public abstract Quaternion GetRotation(Quaternion current);
     public abstract void OnDisable();
 }
--- SecondDraft/Assets/Controls/SingleAxisControls.cs
+++ SecondDraft/Assets/Controls/SingleAxisControls.cs
@@ -12,13 +12,13 @@ public class SingleAxisControls : BaseControls
         
     }
 
-    public override UnityEngine.Vector3 GetMove()
+    public override UnityEngine.Vector3 GetMove(Vector3 current)
     {
-        Vector3 forward = CameraManager.GetCameraForwardVector();
+        Vector3 forward = CameraManager.GetCameraForwardVector().sety(0);
         return forward * Time.deltaTime * walkSpeed * Input.GetAxis("Vertical");
     }
 
-    public override UnityEngine.Quaternion GetRotation()
+    public override UnityEngine.Quaternion GetRotation(Quaternion current)
     {
         float turned = Time.deltaTime * turnSpeed * Input.GetAxis("Horizontal");
         return Quaternion.AngleAxis(turned, Vector3.up);
