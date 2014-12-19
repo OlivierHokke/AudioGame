2261b2d4e72433b6ccdde6838f24684adcadba78
Olivier Hokke <github@wolfox.nl>
2014-12-19 14:17:34 +0100

    Working on a new UI, but temporarily added debug buttons that can enable and disable functions on the fly. Don't worry, it will go and make way for much better stuff. I first want to implement the walk script with footstep sounds.

---
+++ SecondDraft/Assets/CombinedCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombinedCamera : MonoBehaviour {
+
+    public Camera normalCamera;
+    public Camera oculusCamera;
+
+	// Update is called once per frame
+	void Update () {
+        if (oculusCamera.gameObject.activeInHierarchy)
+        {
+            this.transform.rotation = oculusCamera.transform.rotation;
+        }
+        else
+        {
+            this.transform.rotation = normalCamera.transform.rotation;
+        }
+	}
+}
